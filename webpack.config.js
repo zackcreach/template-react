360d845c1490bdac81dca2a75b94effc25144747
Zack Creach <zackcreach@gmail.com>
2017-05-28 08:21:25 -0400

    Initial react template commit.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    // __dirname is a node function specifying the path to the current folder
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
